9b1b87a86df42f43ab79fe328ff5d7fa5c3c9cf6
Guillermo <gbarcomu@alumnos.unex.es>
2016-11-26 13:30:28 +0100

    safdf

--- public/viewCandidates.js
+++ public/viewCandidates.js
@@ -1,18 +1,20 @@
-app.controller("viewCandidatesCtrl",['cfactory', function(cfactory) {
+app.controller("viewCandidatesCtrl",['$http', function($http){
 	var vm = this;
 	vm.candidates = [];
+	var url = 'http://usthackathon.mybluemix.net/getCandidates';
 
 	vm.funciones = {
 
 		getCandidates : function() {
-			cfactory.getCandidates.then(function(respuesta) {
+			$http.get(url)
+			.then(function(respuesta) {
 				console.log("Obteniendo candidatos");
 				vm.candidates = respuesta;
-			}, function(respuesta) {
+			}).catch(error => {
 				console.log("Error obteniendo candidatos");
-			})
+			})	
 		}
 	}
 	
-	//vm.funciones.getCandidates();
+	vm.funciones.getCandidates();
 }]);
